4e5585ffb573744925cdaa44dfd6788b9ca43b06
Legend ModzYT <39273354+ThatLadLegend@users.noreply.github.com>
2023-06-06 13:10:58 +0100

    Website Publish.

---
+++ Website/JS/script.js
@@ -0,0 +1,88 @@
+$(document).ready(function(){
+
+    $('#menu').click(function(){
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('nav-toggle');
+    });
+
+    $(window).on('scroll load',function(){
+        $('#menu').removeClass('fa-times');
+        $('.navbar').removeClass('nav-toggle');
+
+        if(window.scrollY>60){
+            document.querySelector('#scroll-top').classList.add('active');
+        }else{
+            document.querySelector('#scroll-top').classList.remove('active');
+        }
+
+        // scroll spy
+        $('section').each(function(){
+            let height = $(this).height();
+            let offset = $(this).offset().top - 200;
+            let top = $(window).scrollTop();
+            let id = $(this).attr('id');
+
+            if(top>offset && top<offset+height){
+                $('.navbar ul li a').removeClass('active');
+                $('.navbar').find(`[href="#${id}"]`).addClass('active');
+            }
+        });
+    });
+
+    // smooth scrolling
+    $('a[href*="#"]').on('click',function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop : $($(this).attr('href')).offset().top,
+        },500, 'linear')
+    })
+});
+
+document.addEventListener('visibilitychange',
+function(){
+    if(document.visibilityState === "visible"){
+        document.title = "Legend's Portfolio";
+        $("#favicon").attr("href","./Images/lgo.png");
+    }
+    else {
+        document.title = "Come Back To Portfolio";
+      $("#favicon").attr("href","./Images/lgo.png");
+    }
+});
+
+
+// <!-- typed js effect starts -->
+var typed = new Typed(".typing-text", {
+  strings: ["Hey, I'm Legend."],
+  loop: true,
+  typeSpeed: 40,
+  backSpeed: 35,
+  backDelay: 750,
+});
+// <!-- typed js effect ends -->
+
+
+// pre loader end
+
+// disable developer mode
+document.onkeydown = function(e) {
+  if(e.keyCode == 123) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+     return false;
+  }
+}
+
+// Start of Tawk.to Live Chat
+
+// End of Tawk.to Live Chat
